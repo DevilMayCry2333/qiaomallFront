e493f66040b7bf64b92a960fa499c7e9385d0cc1
DevilMayCry2333 <1712363499@qq.com>
2019-08-22 14:09:00 +0800

    商品

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,11 +22,11 @@
         </el-menu-item>
         <el-menu-item index="4">
             <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
+            <span slot="title" @click="orderList">订单列表</span>
         </el-menu-item>
         </el-menu>
     </el-col>
-     <el-col :span="18" v-if="isShow">
+     <el-col :span="18" v-if="isShow==1">
         <el-table
             :data="tableData"
             stripe
@@ -37,25 +37,24 @@
             width="180">
             </el-table-column>
             <el-table-column
-            prop="pic"
-            label="商品图片"
-            width="180">
-                <div class="demo-fit">
-                    <div class="block" v-for="fit in fits" :key="fit">
-                        <el-avatar shape="square" :size="100" :fit="fit" :src="url"></el-avatar>
-                    </div>
-                </div>
-            </el-table-column>
-            <el-table-column
             prop="name"
             label="商品名称">
             </el-table-column>
+            <el-table-column
+            prop="pic"
+            label="商品图片">
+            <template scope="scope">
+            <img :src="scope.row.pic" width="80" height="80" class="head_pic"/>
+            </template>
+
+            </el-table-column>
+
             <el-table-column
             prop="price"
             label="商品价格">
             </el-table-column>
             <el-table-column
-            prop="label"
+            prop="keywords"
             label="标签">
             </el-table-column>
             <el-table-column
@@ -70,38 +69,100 @@
             label="销量">
             </el-table-column>
             <el-table-column
-            prop="verify_status"
+            prop="verifyStatus"
             label="审核状态">
             </el-table-column>
 
         </el-table>
+
         </el-col>
+             <el-col :span="18" v-if="isShow==4">
+        <el-table
+            :data="orderData"
+            stripe
+            style="width: 100%">
+            <el-table-column
+            prop="id"
+            label="编号"
+            width="180">
+            </el-table-column>
+            <el-table-column
+            prop="orderSn"
+            label="订单编号">
+            </el-table-column>
+            <el-table-column
+            prop="createTime"
+            label="提交时间">
+            </el-table-column>
+
+            <el-table-column
+            prop="memberUsername"
+            label="用户账号">
+            </el-table-column>
+            <el-table-column
+            prop="totalAmount"
+            label="订单金额">
+            </el-table-column>
+            <el-table-column
+            prop="payType"
+            label="支付方式">
+            </el-table-column>
+            <el-table-column
+            prop="sourceType"
+            label="订单来源">
+            </el-table-column>
+            <el-table-column
+            prop="status"
+            label="订单状态">
+            </el-table-column>
+
+        </el-table>
+        </el-col>
+
     </el-row>
      
     </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import axios from 'axios'
+const config = require('../config/conf.js');
 export default Vue.extend({
     name:'Home',
     data(){
         return{
-            isShow:false,
+            isShow:0,
             tableData: [
                 {
                 id:1,
-                pic: '2016-05-02',
+                // pic: '2016-05-02',
+                pics:'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
                 name: '王小虎',
                 price:5,
                 label: '标签',
                 sort: 100,
                 sale: 100,
                 verify_status: 1,
-                address: '上海市普陀区金沙江路 1518 弄'
+                address: '上海市普陀区金沙江路 1518 弄',
                 }
             ],
+            orderData:[
+                                {
+                id:2,
+                // pic: '2016-05-02',
+                orderSn:'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+                createTime: '王小虎2',
+                memberUsername:5,
+                totalAmount: '标签',
+                payType: 100,
+                sourceType: 100,
+                status: 1,
+                address: '上海市普陀区金沙江路 1518 弄',
+                }
+
+            ],
             fits: ['fill'],
-            url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+            pic: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
         }
     },
     methods:{
@@ -111,13 +172,50 @@ export default Vue.extend({
       handleClose(key, keyPath) {
         console.log(key, keyPath);
       },
-      goodsList(){
-          this.isShow = true;
+      orderList(){
+          this.isShow = 4;
+          console.log("order");
+                      var that = this;
+                axios({
+                        method:'post',
+                        url: config.url+ "allOrder",
+                        params:{
+                            goodsId:'1'
+                        }
+                }).then(function(resp){
+                        that.$nextTick(function () {
+                        console.log(resp.data);
+                        this.orderData = resp.data.orderData;
+                        // console.log(this.desserts);
+                        })
 
-          console.log("hello");
-      }
+                console.log("hello");
+            })
+
+      },
+        goodsList(){
+            console.log(config);
+            this.isShow = 1;
+            var that = this;
+                axios({
+                        method:'post',
+                        url: config.url+ "AllGoods",
+                        params:{
+                            goodsId:'1'
+                        }
+                }).then(function(resp){
+                        that.$nextTick(function () {
+                        console.log(resp.data);
+                        this.tableData = resp.data.tableData;
+                        // console.log(this.desserts);
+                        })
+
+                console.log("hello");
+            })
+        }
     }
 })
+
 </script>
 
 <style>
---
+++ src/config/conf.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // url : "https://youkaiyu.com:5000/"
+    url: "http://localhost:5000/"
+};
