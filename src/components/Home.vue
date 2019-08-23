556274f6ee902e0b2e224a9e17775878f9d27c2a
DevilMayCry2333 <1712363499@qq.com>
2019-08-23 14:23:33 +0800

    123

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,7 +14,7 @@
         </el-menu-item>
         <el-menu-item index="2">
             <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
+            <span slot="title" @click="goodsCate">商品分类</span>
         </el-menu-item>
         <el-menu-item index="3" disabled>
             <i class="el-icon-document"></i>
@@ -43,7 +43,7 @@
             <el-table-column
             prop="pic"
             label="商品图片">
-            <template scope="scope">
+            <template scope="scope" v-if="isShow==1">
             <img :src="scope.row.pic" width="80" height="80" class="head_pic"/>
             </template>
 
@@ -73,10 +73,75 @@
             label="审核状态">
             </el-table-column>
 
+            <el-table-column
+            label="编辑">
+            <el-button type="primary" icon="el-icon-edit" circle @click="orderEdit"></el-button>
+
+            </el-table-column>
+
+
         </el-table>
 
         </el-col>
-             <el-col :span="18" v-if="isShow==4">
+
+
+     <el-col :span="18" v-if="isShow==2">
+        <el-table
+            :data="cateData"
+            stripe
+            style="width: 100%">
+            <el-table-column
+            prop="id"
+            label="编号"
+            width="180">
+            </el-table-column>
+            <el-table-column
+            prop="name"
+            label="分类名称">
+            </el-table-column>
+            <el-table-column
+            prop="level"
+            label="级别">
+            <template scope="scope" v-if="isShow==1">
+            <img :src="scope.row.pic" width="80" height="80" class="head_pic"/>
+            </template>
+
+            </el-table-column>
+
+            <el-table-column
+            prop="productCount"
+            label="商品数量">
+            </el-table-column>
+            <el-table-column
+            prop="productUnit"
+            label="数量单位">
+            </el-table-column>
+            <el-table-column
+            prop="sort"
+            label="排序">
+            </el-table-column>
+            <el-table-column
+            label="设置">
+            </el-table-column>
+            <el-table-column
+            label="操作">
+            </el-table-column>
+
+
+            <el-table-column
+            label="编辑">
+            <el-button type="primary" icon="el-icon-edit" circle @click="orderEdit"></el-button>
+
+            </el-table-column>
+
+
+        </el-table>
+
+        </el-col>
+
+
+
+    <el-col :span="18" v-if="isShow==4">
         <el-table
             :data="orderData"
             stripe
@@ -116,6 +181,19 @@
             label="订单状态">
             </el-table-column>
 
+            <el-table-column
+            label="删除">
+            <el-button type="danger" icon="el-icon-delete" circle @click="orderDel"></el-button>
+
+            </el-table-column>
+
+            <el-table-column
+            label="查看">
+            <el-button type="info" icon="el-icon-message" circle></el-button>
+
+            </el-table-column>
+
+
         </el-table>
         </el-col>
 
@@ -128,7 +206,7 @@ import Vue from 'vue'
 import axios from 'axios'
 const config = require('../config/conf.js');
 export default Vue.extend({
-    name:'Home',
+    name: 'Home',
     data(){
         return{
             isShow:0,
@@ -136,7 +214,7 @@ export default Vue.extend({
                 {
                 id:1,
                 // pic: '2016-05-02',
-                pics:'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+                level:'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
                 name: '王小虎',
                 price:5,
                 label: '标签',
@@ -146,8 +224,22 @@ export default Vue.extend({
                 address: '上海市普陀区金沙江路 1518 弄',
                 }
             ],
+            cateData: [
+                {
+                id:1,
+                // pic: '2016-05-02',
+                level:'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+                name: '王小虎',
+                productCount:5,
+                productUnit: '标签',
+                sort: 100,
+                sale: 100,
+                verify_status: 1,
+                address: '上海市普陀区金沙江路 1518 弄',
+                }
+            ],
             orderData:[
-                                {
+            {
                 id:2,
                 // pic: '2016-05-02',
                 orderSn:'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
@@ -169,13 +261,52 @@ export default Vue.extend({
       handleOpen(key, keyPath) {
         console.log(key, keyPath);
       },
+      orderDel(){
+          console.log("orderDel");
+           this.$confirm('此操作将永久删除该订单, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
+      },
       handleClose(key, keyPath) {
         console.log(key, keyPath);
+      },
+      goodsCate(){
+           this.isShow = 2;
+          var that = this;
+                axios({
+                        method:'post',
+                        url: config.url+ "goodsCategory",
+                        params:{
+                            goodsId:'1'
+                        }
+                }).then(function(resp){
+                        that.$nextTick(function () {
+                        console.log(resp.data);
+                        this.cateData = resp.data.Data;
+                        // console.log(this.desserts);
+                        })
+
+                console.log("hello");
+            })
+
+
       },
       orderList(){
           this.isShow = 4;
           console.log("order");
-                      var that = this;
+          var that = this;
                 axios({
                         method:'post',
                         url: config.url+ "allOrder",
@@ -191,6 +322,10 @@ export default Vue.extend({
 
                 console.log("hello");
             })
+
+      },
+      orderEdit(){
+          console.log("*****");
 
       },
         goodsList(){
